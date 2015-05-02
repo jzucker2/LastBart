119819e3490f31cadf2219b99f5ead617b77d712
Jordan Zucker <jordan.zucker@gmail.com>
2015-05-02 15:44:00 -0700

    Set up basic LastBart project

---
+++ LastBart/LastBart/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LastBart
+//
+//  Created by Jordan Zucker on 5/2/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LastBart/LastBart/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  LastBart
+//
+//  Created by Jordan Zucker on 5/2/15.
+//  Copyright (c) 2015 Jordan Zucker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
